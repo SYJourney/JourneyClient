ed950827f255f13dc52f36a812dc59d04d88d795
Pharaoh <pharaoh@PharaohdeMacBook-Pro.local>
2017-02-23 15:53:30 +0800

    fix character will jump out of some map

--- Gameplay/Physics/Footholdtree.cpp
+++ Gameplay/Physics/Footholdtree.cpp
@@ -1,6 +1,6 @@
 /////////////////////////////////////////////////////////////////////////////
 // This file is part of the Journey MMORPG client                           //
-// Copyright © 2015-2016 Daniel Allendorf                                   //
+// Copyright ï¿½ 2015-2016 Daniel Allendorf                                   //
 //                                                                          //
 // This program is free software: you can redistribute it and/or modify     //
 // it under the terms of the GNU Affero General Public License as           //
@@ -243,6 +243,13 @@ namespace jrc
 		{
 			phobj.fhlayer = nextfh.layer();
 		}
+        
+        if(phobj.fhid == 0)
+        {
+            phobj.fhid = curfh.fhid;
+            phobj.limitx(curfh.x1();
+        }
+        
 	}
 
 	const Foothold& Footholdtree::get_fh(uint16_t fhid) const
